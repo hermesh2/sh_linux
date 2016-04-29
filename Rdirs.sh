42325034b9101e00bdb432334f60a539de54f23a
Chabi <javier.villacampa.gonzalez@gmail.com>
2016-04-29 15:32:29 +0200

    Add new Rdirs.sh

--- Rdirs.sh
+++ Rdirs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# cd $V1
+# cd $1
+echo "=========================="
+echo "Refresh R GitHub"
+echo "=========================="
+rm -rf ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/
+git clone https://github.com/hermesh2/RBasics_Sh.git  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/
+
+
 echo "=========================="
 echo "Create dirs"
 echo "=========================="
@@ -12,10 +19,12 @@ mkdir Documents
 mkdir Results
 mkdir RData
 
+
 echo "=========================="
 echo "Copy the basic script"
 echo "=========================="
-cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/R/01/Basic.R ./Scripts/Basic.R
-ln -s  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/R/01/cleanNonEnglishCharacters.R Scripts/Functions/cleanNonEnglishCharacters.R
+cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/Basic.R ./Scripts/Basic.R
+cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/00_Basic 00_Basic
+ln -s  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/cleanNonEnglishCharacters.R Scripts/Functions/cleanNonEnglishCharacters.R
 sudo apt-get install xclip
-ln -s ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/R/01/clipboard.R Scripts/Functions/clipboard.R
+ln -s ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/clipboard.R Scripts/Functions/clipboard.R
