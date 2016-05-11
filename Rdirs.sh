b0abdfd467bef029d172849720528f56fd37f59b
Chabi <jvillacampa@geoblink.com>
2016-05-11 16:47:12 +0200

    Copy the script clean in Rdirs. In order to avoid problems. The link is not enought

--- Rdirs.sh
+++ Rdirs.sh
@@ -18,6 +18,11 @@ echo "Copy the basic script"
 echo "=========================="
 cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/Basic.R ./Scripts/Basic.R
 cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/00_Basic.R 00_Basic.R
-ln -s  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/cleanNonEnglishCharacters.R Scripts/Functions/cleanNonEnglishCharacters.R
+# ln -s  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/cleanNonEnglishCharacters.R Scripts/Functions/cleanNonEnglishCharacters.R
+<<<<<<< Updated upstream
+cp  ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/cleanNonEnglishCharacters.R Scripts/Functions/cleanNonEnglishCharacters.R
+=======
+cp ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/RBasics_Sh/00_Basic.R 00_Basic.R
+>>>>>>> Stashed changes
 sudo apt-get install xclip
 ln -s ~/Dropbox/Chabi/R/Scripts\ de\ R/Basic_Scripts/clipboard.R Scripts/Functions/clipboard.R
